cbd1d63f2a7302b05a9fee3cf9b1e8d86a44ff2b
Igor Potseluev <igor.potceluev@fungible.com>
2022-10-14 19:20:08 +0300

    Iteration3: more detailed skeleton

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import asyncio
 import csv
+from re import L
 import typing
 
 import src.cmd_line_parser as cmd_line_parser
@@ -13,7 +14,14 @@ class CourseInfo(typing.NamedTuple):
     number_of_ratings: int
 
 
-async def gather_courses_data(category_name: str, workers_count: int) -> list[CourseInfo]:
+baseurl = 'https://www.coursera.org/browse/'
+
+
+async def gather_courses_links(http_client, category_name: str) -> list[str]:
+    return ['link1', 'link2']
+
+
+async def gather_courses_data(http_client, courses_links: list[str], workers_count: int):
     return [
         CourseInfo(name='Course1', provider='Provider1', description='Description1', number_of_students=1, number_of_ratings=1),
         CourseInfo(name='Course2', provider='Provider2', description='Description2', number_of_students=2, number_of_ratings=2)
@@ -21,18 +29,23 @@ async def gather_courses_data(category_name: str, workers_count: int) -> list[Co
 
 
 async def main():
+    # 1. Parse command line arguments
     parser = cmd_line_parser.Parser()
-
-    courses_list = await gather_courses_data(
-        category_name=parser.get_category_name(),
+    # 2. Create http client
+    http_client = None
+    # 3. Gather list of links to courses
+    courses_links = await gather_courses_links(http_client, category_name=parser.get_category_name())
+    # 4. Collect courses info using max(workers_count, len(links_list) task pool
+    courses_data = await gather_courses_data(
+        http_client,
+        courses_links=courses_links,
         workers_count=parser.get_workers_count()
     )
-
-    # Write to csv
+    # 5. Write to csv
     with open(parser.get_output_file_path(), 'w', newline='') as f:
         csv_writer = csv.writer(f)
-        csv_writer.writerow(courses_list[0]._fields)
-        csv_writer.writerows(courses_list)
+        csv_writer.writerow(courses_data[0]._fields)
+        csv_writer.writerows(courses_data)
 
 
 if __name__ == '__main__':
