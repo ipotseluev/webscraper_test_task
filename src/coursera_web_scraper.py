dcf31547a467a0b70ee5c2b5d9d7716ab8dbd4ab
Igor Potseluev <igor.potceluev@fungible.com>
2022-10-18 19:48:31 +0300

    Add naive workers implementation

--- src/coursera_web_scraper.py
+++ src/coursera_web_scraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import asyncio
 import csv
 import typing
 
@@ -18,17 +19,29 @@ class CourseraWebScraper:
     def __init__(self, async_http_reader: AsyncHttpReader):
         self.BASE_URL = 'https://www.coursera.org'
         self.async_http_reader = async_http_reader
+        self.task_counter = 0
+        self.links_count = 0
 
     async def _get_courses_links(self, courses_category: str) -> list[str]:
+        """
+        Returns list of links to courses of given category
+        """
         category_browse_url = f"{self.BASE_URL}/browse/{courses_category}"
         html_text = await self.async_http_reader.get_text(url=category_browse_url)
         soup = BeautifulSoup(html_text, "html.parser")
         product_cards = soup.findAll('div', class_='rc-ProductCard')
         courses_links = [card.find('a', class_='CardText-link').get('href') for card in product_cards]
+        self.links_count = len(courses_links)
         return courses_links
 
-    async def _get_course_data(self, course_link: str, course_category: str) -> CourseInfo:
+    async def _get_course_data(self, course_link: str) -> CourseInfo:
+        """
+        Downloads html of a course and extracts necessary info
+        """
         link = f'{self.BASE_URL}{course_link}'
+        self.task_counter += 1
+        print(f"[{self.task_counter}/{self.links_count}] Processing {link}")
+
         page_text = await self.async_http_reader.get_text(url=link)
         course_scrapper = CoursePageScraper(html=page_text)
 
@@ -47,17 +60,27 @@ class CourseraWebScraper:
             students_count=students_count,
             ratings_count=ratings_count
         )
+
     async def _collect_courses_data(
-            self,
-            courses_links: list[str],
-            courses_category: str,
-            workers_count: int
-        ) -> list[CourseInfo]:
-        result = []
-        for i in range(len(courses_links)):
-            print(f"[{i + 1}/{len(courses_links)}] Processing {courses_links[i]}")
-            result.append(await self._get_course_data(course_link=courses_links[i], course_category=courses_category))
-        return result
+        self,
+        courses_links: list[str],
+        workers_count: int
+    ):
+        """
+        Executes tasks for gathering info about courses and returns a list of information about courses
+        """
+        self.task_counter = 0
+        courses_info: list[CourseInfo] = []
+        while courses_links:
+            tasks = []
+            for i in range(min(workers_count, len(courses_links))):
+                link = courses_links.pop(0)
+                task = asyncio.create_task(
+                        self._get_course_data(course_link=link)
+                )
+                tasks.append(task)
+            courses_info += await asyncio.gather(*tasks)
+        return courses_info
 
     async def scrap_courses_info(self, courses_category: str, workers_count: int, output_file_path: str):
         # 1. Collect links to courses from the given category
@@ -65,7 +88,6 @@ class CourseraWebScraper:
 
         # 2. Collect courses info using max(workers_count, len(links_list) task pool
         courses_data = await self._collect_courses_data(
-            courses_category=courses_category,
             courses_links=courses_links,
             workers_count=workers_count
         )
